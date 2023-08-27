e5a38b15fbafed0bcd45c9e1a38fff044f393bb0
trian damai <triannurizkillah@gmail.com>
2023-08-27 21:50:15 +0700

    feat: create feature upload attachment task
    jira: GAWE-121

--- src/main/java/com/bluehabit/eureka/controller/TaskController.java
+++ src/main/java/com/bluehabit/eureka/controller/TaskController.java
@@ -10,8 +10,10 @@ package com.bluehabit.eureka.controller;
 import com.bluehabit.eureka.common.BaseResponse;
 import com.bluehabit.eureka.common.PageResponse;
 import com.bluehabit.eureka.component.data.Task;
+import com.bluehabit.eureka.component.data.TaskAttachment;
 import com.bluehabit.eureka.component.data.TaskPriority;
 import com.bluehabit.eureka.component.data.TaskStatus;
+import com.bluehabit.eureka.component.model.UploadAttachmentRequest;
 import com.bluehabit.eureka.services.TaskPriorityService;
 import com.bluehabit.eureka.services.TaskService;
 import com.bluehabit.eureka.services.TaskStatusService;
@@ -20,6 +22,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,5 +65,16 @@ public class TaskController {
     )
     public ResponseEntity<BaseResponse<Task>> createTemporaryTask() {
         return taskService.createTemporaryTask();
+    }
+
+    @PostMapping(
+        path = "upload-attachment",
+        produces = MediaType.APPLICATION_JSON_VALUE,
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<BaseResponse<TaskAttachment>> uploadAttachment(
+        @ModelAttribute UploadAttachmentRequest request
+    ) {
+        return taskService.uploadAttachment(request);
     }
 }
--- src/main/java/com/bluehabit/eureka/services/TaskService.java
+++ src/main/java/com/bluehabit/eureka/services/TaskService.java
@@ -9,9 +9,14 @@ package com.bluehabit.eureka.services;
 
 import com.bluehabit.eureka.common.AbstractBaseService;
 import com.bluehabit.eureka.common.BaseResponse;
+import com.bluehabit.eureka.common.FileUtil;
+import com.bluehabit.eureka.component.AttachmentType;
 import com.bluehabit.eureka.component.data.Task;
+import com.bluehabit.eureka.component.data.TaskAttachment;
+import com.bluehabit.eureka.component.data.TaskAttachmentRepository;
 import com.bluehabit.eureka.component.data.TaskRepository;
 import com.bluehabit.eureka.component.data.TaskStatusRepository;
+import com.bluehabit.eureka.component.model.UploadAttachmentRequest;
 import com.bluehabit.eureka.exception.GeneralErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,12 +28,16 @@ import java.util.UUID;
 
 @Service
 public class TaskService extends AbstractBaseService {
+    private static final int maxAttachment = 3;
 
     @Autowired
     private TaskStatusRepository taskStatusRepository;
 
     @Autowired
     private TaskRepository taskRepository;
+
+    @Autowired
+    private TaskAttachmentRepository taskAttachmentRepository;
 
     public ResponseEntity<BaseResponse<Task>> createTemporaryTask() {
         return getAuthenticatedUser(user -> {
@@ -53,5 +62,38 @@ public class TaskService extends AbstractBaseService {
         }, () -> {
             throw new GeneralErrorException(HttpStatus.BAD_REQUEST.value(), translate("task.create.temp.user.not.allowed"));
         });
+    }
+
+    public ResponseEntity<BaseResponse<TaskAttachment>> uploadAttachment(
+        UploadAttachmentRequest request
+    ) {
+        validate(request);
+        return taskRepository.findById(request.taskId())
+            .map(task -> {
+                if (task.getAttachments().size() >= maxAttachment) {
+                    throw new GeneralErrorException(HttpStatus.FORBIDDEN.value(), translate("task.attachment.upload.failed.max"));
+                }
+                final String idAttachment = UUID.randomUUID().toString();
+                final OffsetDateTime currentDate = OffsetDateTime.now();
+                final TaskAttachment taskAttachment = new TaskAttachment();
+                taskAttachment.setId(idAttachment);
+                taskAttachment.setMimeType(request.file().getContentType());
+                taskAttachment.setTask(task);
+                taskAttachment.setType(AttachmentType.IMAGE);
+                taskAttachment.setCreatedAt(currentDate);
+                taskAttachment.setUpdatedAt(currentDate);
+
+                return FileUtil.saveFile(request.file(), idAttachment).map(fileName -> {
+                    taskAttachment.setName(fileName);
+                    final TaskAttachment savedTaskAttachment = taskAttachmentRepository.save(taskAttachment);
+                    return BaseResponse.success(translate("task.attachment.upload.success"), savedTaskAttachment);
+                }).orElseThrow(() -> {
+                    return new GeneralErrorException(
+                        HttpStatus.BAD_REQUEST.value(),
+                        translate("task.attachment.upload.file.not.uploaded")
+                    );
+                });
+            })
+            .orElseThrow(() -> new GeneralErrorException(HttpStatus.BAD_REQUEST.value(), translate("task.attachment.upload.task.not.found")));
     }
 }
